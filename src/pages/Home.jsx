9b0b029b1aa65ba379ec4dcc19ca2bc9e7c38a49
Fabio Filho <fabio.filho@acct.global>
2022-04-10 14:53:41 -0300

    change toml config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import GameContextProvider from '../contexts/GameContext';
 import Board from '../components/Board';
 
 export default function Home() {
+  fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
+  .then(() => console.log('Successful'))
+  .catch(() => console.log('HTTP WRONG: '))
   return (
     <GameContextProvider >
       <Board />    
